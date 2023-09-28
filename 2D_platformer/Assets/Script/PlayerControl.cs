161204adf0f31d9cec250c3c0063bced40bc7cce
Akshat Solanki <akshat2543@gmail.com>
2023-09-28 19:15:19 +0530

    changes

--- 2D_platformer/Assets/Script/PlayerControl.cs
+++ 2D_platformer/Assets/Script/PlayerControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
    public float speed =300f;
    public float jumpForce=200f;
