a33a2a57ffb8df6396f043cded315e0877d81bb2
Akshat Solanki <akshat2543@gmail.com>
2023-09-28 19:50:39 +0530

    cinemachine

---
+++ 2D_platformer/Assets/Script/playercontroller.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+   public float speed =300f;
+   public float jumpForce=200f;
+    float horizontalInput;
+    public Rigidbody2D rb;
+    public bool isOnGround = true;
+    public SpriteRenderer spriteRenderer;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();  
+        spriteRenderer=GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // for translation along x axis.
+        horizontalInput= Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right*speed*Time.deltaTime*horizontalInput);
+
+        // for rotating character
+        if(horizontalInput<0){
+            spriteRenderer.flipX=true;
+        }
+        if(horizontalInput>0) {spriteRenderer.flipX=false;
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround){
+            rb.AddForce(Vector3.up*jumpForce,ForceMode2D.Impulse);
+        isOnGround= false;
+        }
+
+        // else if (collision.gameObject.CompareTag("Obstacle")){
+        //     Debug.Log("GameOver");
+        //     isGameOver=true;
+        // }
+        
+    }
+    public void OnCollisionEnter2D ( Collision2D collision ){
+        if(collision.gameObject.CompareTag("Ground")){
+            isOnGround=true;    
+          
+        }
+        }
+       
+
+}
